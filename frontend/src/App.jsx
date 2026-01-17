ed4b3ea8d083c69865389e80d975f686af9672f1
Saad Arqam <saadarqamrnc@gmail.com>
2026-01-17 11:52:34 +0530

    feat: initialize Web Audio pipeline with 16kHz mic capture and PCM frame processing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { useEffect } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  useEffect(()=>{
+    async function initMic(){
 
+      // mic permission
+      const stream=await navigator.mediaDevices.getUserMedia({ audio: true });
+      console.log("Audio permission granted!!!")
+
+      // noisse supression
+      const audioContext=new AudioContext({
+        sampleRate:16000,
+      })
+
+      const source=audioContext.createMediaStreamSource(stream)
+      console.log("AudioContext sample rate:", audioContext.sampleRate);
+
+      // capturing raw audio files
+      const processor=audioContext.createScriptProcessor(4096,1,1)
+      source.connect(processor)
+      processor.connect(audioContext.destination)
+
+      processor.onaudioprocess=(e)=>{
+        const input=e.inputBuffer.getChannelData(0);
+        console.log("Audio frame", input.length)
+        let sum = 0;
+
+
+        // sanity check
+        for (let i = 0; i < input.length; i++) {
+          sum += Math.abs(input[i]);
+        }
+        console.log("Energy:", sum / input.length);
+
+      }
+    }
+    initMic()
+  },[])
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Voice Assistant</h1>
+    </div>
   )
 }
 
